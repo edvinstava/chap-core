972b940c10b09d5c95e624cfa3985354b1a57147
edvinstava <edvin@devotta.no>
2026-04-28 20:25:10 +0200

    fix: make xai migration revision chain linear

--- alembic/versions/d4e5f6a7b8c9_add_xai_tables.py
+++ alembic/versions/d4e5f6a7b8c9_add_xai_tables.py
@@ -6,8 +6,8 @@ Adds:
 - provides_native_shap boolean column on modeltemplatedb so configured models can
   declare they emit shap_values.csv at predict time.
 
-Revision ID: b2c3d4e5f6a7
-Revises: a1b2c3d4e5f6
+Revision ID: d4e5f6a7b8c9
+Revises: c3d4e5f6a7b8
 Create Date: 2026-04-27
 
 """
@@ -18,8 +18,8 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "b2c3d4e5f6a7"
-down_revision: Union[str, Sequence[str], None] = "a1b2c3d4e5f6"
+revision: str = "d4e5f6a7b8c9"
+down_revision: Union[str, Sequence[str], None] = "c3d4e5f6a7b8"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
